2d7f5fc0af9c998ece8b3a81f3b3ac4e2cdb53d8
Qiu-zzz <756597346@qq.com>
2019-12-19 14:20:22 +0800

    init repo

---
+++ src/main/java/communityselfproject/CommunitySelfProjectApplication.java
@@ -0,0 +1,12 @@
+package communityselfproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySelfProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitySelfProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/communityselfproject/CommunitySelfProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package communityselfproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommunitySelfProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
